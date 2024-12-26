d6d495493d755747f565d3fd3429a6ad280964a2
Ethan Wang <ethanwang@EthandeMacBook-Pro.local>
2024-12-26 23:38:01 +0800

    - Updated layout.tsx to enhance the admin panel UI with a sidebar and header. - Introduced new files for dashboard filtering and table components. - Added a new page for displaying goods and a store page. - Enhanced the dashboard page with a user section for better organization.

--- src/app/admin/(panel)/dashboard/page.tsx
+++ src/app/admin/(panel)/dashboard/page.tsx
@@ -2,6 +2,12 @@ export default function AdminPanel() {
     return (
         <div>
             <h1>管理员面板</h1>
+            <div>
+                <div>
+                    <h1>用户</h1>
+                </div>
+            </div>
+
         </div>
     )
 } 
--- src/app/admin/(panel)/layout.tsx
+++ src/app/admin/(panel)/layout.tsx
@@ -28,9 +28,22 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         ]}
     />
     return (
-
         <div>
-            {children}
+            <div className="fixed left-0 top-0 h-screen w-64 bg-[#f5f5f5] text-white p-4">
+                <div className="mb-8">
+                    <h1 className="text-2xl font-bold text-center text-[#0ea5e9]">管理面板</h1>
+                </div>
+                {menu}
+                <div className="flex absolute bottom-4 text-black">
+                    logout
+                </div>
+            </div>
+            <div className="fixed top-0 left-64 w-[calc(100%-16rem)] h-16 bg-[#f5f5f5] flex items-center z-10">
+                NEXTJS01
+            </div>
+            <div className="ml-64 flex-1 p-8 pt-20 min-h-screen bg-white">
+                {children}
+            </div>
         </div>
     )
 }
