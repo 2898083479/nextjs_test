4b63fd504e2c210433b77304cb467a0c8d07f329
ethan <2898083479@qq.com>
2025-02-20 10:25:43 +0800

    Enhance store edit dialog with status selection and date picker

--- src/app/admin/(panel)/dashboard/store/dialog/edit/index.tsx
+++ src/app/admin/(panel)/dashboard/store/dialog/edit/index.tsx
@@ -1,15 +1,29 @@
 import EditDialog from "./edit-dialog";
-
+import SuccessDialog from "./success-dialog";
+import { Store } from "../../types";
+import { EditStep, useEditStore } from "../../store";
 interface Props {
     open: boolean;
     onOpenChange: (open: boolean) => void;
+    data: Store;
 }
 
-export default function EditIndexDialog({ open, onOpenChange }: Props){
+export default function EditIndexDialog({ open, onOpenChange, data }: Props) {
+    const { step } = useEditStore()
+    if (step === EditStep.Success) {
+        return (
+            <SuccessDialog
+                open={open}
+                onOpenChange={onOpenChange}
+            />
+        )
+    }
+
     return (
         <EditDialog
             open={open}
             onOpenChange={onOpenChange}
+            data={data}
         />
     )
 }
---
+++ src/app/admin/(panel)/dashboard/store/dialog/edit/success-dialog.tsx
@@ -0,0 +1,30 @@
+import WrapperDialog from "@/components/core/wrapper-dialog/wrapper-dialog";
+import { Button } from "@/components/ui/button";
+interface Props {
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+export default function SuccessDialog({ open, onOpenChange }: Props) {
+    return (
+        <WrapperDialog
+            open={open}
+            onOpenChange={onOpenChange}
+        >
+            <div className="flex flex-col gap-4">
+                <div className="text-[16px] font-[600] text-[#101828]">編輯成功</div>
+                <div className="text-[14px] text-[#5D7285]">您已成功編輯商店信息</div>
+                <div className="flex justify-end">
+                    <Button
+                        type="button"
+                        variant="outline"
+                        className="bg-[#0C7FDA] text-white hover:bg-[#0C7FDA]/80"
+                        onClick={() => onOpenChange(false)}
+                    >
+                        確定
+                    </Button>
+                </div>
+            </div>
+        </WrapperDialog>
+    )
+}
